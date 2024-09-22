bfe1b734c4a8767e30aa278f4a09c91cde06a63c
joshrpg <46099965+joshrpg@users.noreply.github.com>
2024-09-22 14:53:29 -0500

    fix logic and typos

--- pterodactyl/files/nginx-rtmp/startup.sh
+++ pterodactyl/files/nginx-rtmp/startup.sh
@@ -1,11 +1,11 @@
 #!/bin/ash
-if [ -f /home/container/tmp ]; then
+if [ ! -d /home/container/tmp ]; then
 	mkdir /home/container/tmp
 else
   rm -rf /home/container/tmp/*
 fi
 
-if [ -f /home/container/tmp ]; then
+if [ ! -d /home/container/logs ]; then
   mkdir /home/container/logs;
 fi
 
