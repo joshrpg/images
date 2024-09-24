db911e21a3d8da3cf353caca540a01aa5c6a12d5
joshrpg <46099965+joshrpg@users.noreply.github.com>
2024-09-23 20:20:36 -0500

    update startup to clean web dir

--- pterodactyl/files/nginx-rtmp/startup.sh
+++ pterodactyl/files/nginx-rtmp/startup.sh
@@ -9,5 +9,11 @@ if [ ! -d /home/container/logs ]; then
   mkdir /home/container/logs;
 fi
 
+if [ ! -d /home/container/web ]; then
+  mkdir /home/container/web;
+else
+  rm -rf /home/container/web/*
+fi
+
 envsubst < nginx.conf.template > nginx.conf
 /usr/sbin/nginx -c /home/container/nginx.conf -p /home/container
